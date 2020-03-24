29b67c14def3f74a2a5067dc91aa50efe7aa957b
Jay <Yuweh@users.noreply.github.com>
2020-03-24 14:11:02 +0800

    complete ⊂(◕.◕⊂)

--- Data Science/Project SPARTA/Week 1/week1.1TOweek1.5.py
+++ Data Science/Project SPARTA/Week 1/week1.1TOweek1.5.py
@@ -38,5 +38,278 @@
 			This is the data value chain: from the birth of data, to information (answering the question, “What happened?”), to insights (answering the questions, “Why did it happen?
 			What could likely happen next?”), to imperatives or actionable insights (answering the question, “What should be done next?”).
 
+	- 1.3.2 Watch: Data Value in Retail - An Example
+			Let us look into an example on how the data value chain is
+			realized.
+			
+			Supermarkets, department stores, online stores, even our
+			suking sari-sari store all perform Analytics and allow data
+			to move up its value chain.
+			
+			Whenever and wherever we go shopping, data about our
+			purchase is recorded: date and time, location, the item
+			bought, any discount, total cost, and, at times, our data as
+			a customer.
+			
+			All these are collected on the cash register, or
+			on an ecommerce site, or in Aling Maria’s notebook.
+			This is the data stage.
+			
+			At the end of the day, the shop owner would want to
+			know, “What happened?
+			How much sales did they make?”
+			Consolidating all the day’s sales would answer these
+			questions.
+			
+			As the shop owner would also have the information from previous days or months,
+			he or she might also notice that the sales for the day is
+			quite less from previous days.
+			
+			This is also a piece of information that the data could tell the shop owner.
+			This is the information stage.
+			The shop owner would then naturally ask, “Why did it
+			happen?”
+			
+			The shop owner would want to know what caused the dip in sales and if there’s any
+			indication that it might continue in the future, the question,
+			“What could likely happen next?”
+			
+			Looking at historical data, the shop owner might potentially see that the dip in
+			sales always occur in the same period of the year.
+			Further investigation could show that there are certain products
+			stocked in their inventory that have really low sales during
+			this time of the year.
+			
+			These are insights that a shop owner can deduce from the historical information.
+			This is the insights stage.
+			
+			Finally, now knowing what caused the dip in sales and that
+			there appears to be a pattern every year on the same
+			period, the shop owner will ask, “What should be done
+			next?”
+			
+			Knowing what products would sell more at given times of the year, the shop owner can then
+			take action on the insights that were derived.
+			This is the imperatives stage.
+			
+			With new actions taken to improve their business, the shop
+			owner goes into a new cycle of the data value chain.
+			
+	- 1.3.3 Watch: Common Analytics Terminologies
+			
+			From the data value chain, certain Analytics disciplines
+			emerge.
+			
+			These are Analytics terminologies that you may
+			have already heard of.
+			
+			Normally associated with Data are Data Governance, Data
+			Management, Data Security, and Data Ethics.
+			
+			These are disciplines that call for deep knowledge about
+			what data is being collected, how it is being collected,
+			how it is used, who has access to it, among others.
+			
+			With new government regulations on data handling, organizations
+			are being mandated to tighten the performance of these
+			activities.
+			
+			Associated with Information are terms such as Data
+			Engineering, Data Warehousing, Data Architecture, Business Intelligence.
+			And since this stage answers the question, “What happened?”, this is where
+			Descriptive Analytics is also done.
+			
+			With Insights, as we answer the questions, “Why did it
+			happen?
+			
+			What could likely happen next?”, this is where
+			encounter terminologies such as Data Mining, Algorithms,
+			Machine Learning.
+			This is also where we do Diagnostic Analytics and Predictive Analytics.
+			
+			Finally, with Imperatives, we answer the question, “What
+			should be done next?”, this is where we encounter
+			terminologies such as Optimization, Simulation, Recommendation Engines.
+			This is also where we do Prescriptive Analytics.
+			Confused?
+			Overwhelmed?
+			That’s ok.
+			
+			We’ll get to learn more of these Analytics disciplines in the
+			next module and in the other courses of Project SPARTA.
+			
+	- 1.3.4 Watch: The Major Analytics Job Families
+			As Analytics transforms data to information to insights to
+			imperatives through the value chain, we now find the
+			major roles in Analytics.
+			
+			Data Stewards develop, enforce, and maintain an
+			organization’s data governance process, data usage, and data security policies to
+			ensure that data assets provide the organization with high-quality data.
+			Their expertise is in business and industry domains.
+			
+			Data Stewards are the data gatekeepers of an organization.
+			You won’t be able to get data without going through them.
+			As they also ensure data quality, they will be the most
+			knowledgeable in determining how to deal with missing or
+			fix unclean data.
+			Related job titles include data privacy officer, data security
+			officer, data governance manager, data curator, data
+			librarian.
+			
+			Data Engineers design, construct, test, and maintain data
+			infrastructures including applications that extract, clean,
+			transform, and load data from the data sources to
+			centralized data repositories.
+			Their expertise is in information technology, information science,
+			and computer science.
+			
+			Ensuring that data from the centralized data repositories
+			are in-sync with the various data sources is the
+			responsibility of the Data Engineers.
+			Furthermore, they are also responsible to make sure that the data
+			infrastructures are available to key stakeholders during agreed
+			times.
+			Related job titles include ETL developer, data architect,
+			data warehousing professional, big data engineer.
+			
+			Data Scientists leverage statistical techniques and create
+			analytical models to derive new insights from quantitative
+			and qualitative data.
+			Their expertise is in mathematics and statistics.
+			Data Scientists have keen eyes to find trends and patterns
+			from current and historical pieces of information.
+			This allows Data Scientists to make predictions
+			on what could potentially happen next.
+			Related job titles include statistician, statistical modeler,
+			advanced analytics professional.
+			
+			Functional Analysts utilize data and leverage on derived
+			insights to help organizations make better decisions on a
+			specific functional domain.
+			Their expertise is in business and industry domains.
+			While it is still the role of Data Scientists to come up with
+			prescriptive models and algorithms, the Functional Analysts, with their domain expertise, will
+			be the one who will validate the insights derived by the
+			Data Scientists.
+			They will then make the final prescriptions to the
+			leadership team enabling the leaders of the organizations
+			to make better decisions.
+			Related job titles include research analyst, human resource
+			analyst, marketing analyst, financial analyst, operations
+			analyst.
+			
+			Analytics Managers develop and guide data-driven projects
+			– from initiation to planning, execution to performance
+			monitoring, to closure.
+			Their expertise is in project management.
+			Analytics Managers bring the Analytics team together.
+			With knowledge on what to expect from each team
+			member, the Analytics Managers ensure the successful
+			delivery of Analytics projects.
+			
+			Related job titles include chief data officer, project
+			manager, data engineering manager, data science manager, analytics translator.
+			It should be recognized that there are numerous Analytics
+			job titles out there and that there are overlaps across these
+			roles.
+			
+			The Analytics Association of the Philippines believes,
+			however, that the job families identified here are
+			differentiated enough especially in their role in the data
+			value chain and in the identified areas of expertise.
+			Such jobs require people with unique skills and experience
+			to fill these roles.
+			
+			By understanding this distinction, organizations and Analytics practitioners
+			can share the same set of expectations to ensure that, one,
+			organizations get the most out of their Analytics efforts
+			and, two, that Analytics practitioners are positively engaged
+			by performing tasks that are aligned to their
+			profession and career path.
 
+			To know more about the different Analytics Roles, go to the following links:
+			- Data Steward
+			- Data Engineer
+			- Data Scientist
+			- Functional Analyst
+			- Analytics Manager
+
+	- 1.3.5 Watch: Decision Support Systems
+
+			As we proceed to come to a shared definition of Analytics,
+			let’s look at a definition of Analytics that states that
+			Analytics is “a decision support system”.
+			
+			We still elect to use this term – decision support – because
+			while Analytics can provide the end user with data, information, insights,
+			and prescribed actions, we should maintain that the end
+			user can still choose to act upon them or not.
+			
+			The final decision is still with the end user.
+			
+			With fear spreading about how digital technologies such as
+			artificial intelligence, robotic process automation, and the
+			like will take over our jobs, this is important to emphasize.
+			
+			Analytics can only provide decision support.
+			We, the end user, will still have the final say.
+			
+			What we are adding to this definition, however, is that, in
+			this digital age, Analytics provides decision support not
+			only to humans within an organization, but also to digital
+			processes.
+			
+			Analytics is also about the provisioning of data,
+			information, and insights to drive digitalized processes in
+			an intelligent way.
+			
+			Smart appliances, self-driving cars, manufacturing robots have digital processes
+			that are supported by Analytics.
+
+	- 1.3.6 Watch: Analytics Defined
+
+			With this understanding of the data value chain, the major job families, and decision
+			support systems, the following definition of Analytics is derived:
+			
+			Analytics progresses data along its value chain, with the purpose of delivering the
+			right decision support to the right people and digital processes at the right time . . . for
+			the greater good.
+			
+			In this definition, we note the work that Analytics is expected to perform, and the
+			recipients of its output.
+			
+			More importantly, while Analytics – and other technologies and disciplines, for that
+			matter – is, in itself, amoral, the definition emphasizes the ethical use of Analytics.
+
+
+- 1.4 Exit Assessment
+	- Quiz This content is graded
+			
+
+
+- 1.5 In a Nutshell
+
+			That’s all for Week 1: Analytics, the Data Value Chain,
+			and the Analytics Roles.
+			
+			Here are our key take-away points from this week:
+			
+			● Analytics mines and refines data to extract its value
+			as it transforms data to information, to insights, and
+			to imperatives.
+			
+			● This transformation is performed through the
+			various Analytics roles: data steward, data engineer,
+			data scientist, functional analyst, and analytics manager.
+			It is important that the right people are assigned to the right roles.
+			
+			● Analytics is still just a decision support system that
+			enables us, the end user, to make the final ethical
+			decision on what the data is telling us.
+			In Week 2, we will be discussing the Analytics
+			Competencies and the Professional Maturity Model.
+			We will then see the skills needed to perform
+			the roles that we have defined in this week.
+			Thank you for completing Week 1 and see you in Week 2.
 */
