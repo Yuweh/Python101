26dbe1b9231cd6aff77fb50c5a6e0c097b8a8949
Jay <Yuweh@users.noreply.github.com>
2020-03-24 10:48:54 +0800

    Up to week 1.3 added

---
+++ Data Science/Project SPARTA/Week 1/week1.1TOweek1.5.py
@@ -0,0 +1,42 @@
+/*
+//====================================================================================
+//  (っ˘▽˘)っ Week 1 TEXT DATA : Analytics, the Data Value Chain, and the Analytics Roles ⊂(◕.◕⊂)
+//====================================================================================
+- 1.1 Course Introduction
+- 1.2 Introduction
+- 1.3 Coming to a Common Definition of Analytics
+	- 1.3.1 Watch: Data Value Chain
+
+			To come to a shared definition of Analytics, let’s start with the data value chain.
+			
+			Data has been called a lot of things – a new natural resource, the new oil, the new water and so on and so forth.
+			Being so, data then has to be mined and refined to extract the value from it. Analytics enables this transformation.
+			And it starts, of course, with the creation of data. At this stage, data is created and generated from its source,
+			mostly, from us: personal biodata, an employment application form, a bank loan application form, our medical records.
+			When we buy something either from a physical store or an online store, when we call or text someone, when we take a photo, when we create videos, when we post something on Facebook or on Twitter, we create data.
+			Machines and equipment can also create data: biometric machines, CCTVs, barcode scanners in retail stores, our phones, our computers. 
+			This is the birth of data.
+			
+			All of these are collected by various people and organizations, stored into their applications, waiting to be extracted for the value that they will provide.
+			The first transformation that data goes through is when it is extracted from these various sources and consolidated into a single repository.
+			In this process, data is cleaned, categorized, transformed, aggregated, and loaded into a single repository where similar data from the past was also stored.
+			At this stage, data is transformed into information.
+			
+			At this stage as well, organizations will be able to answer the question, “What happened?”
+			The second transformation that data goes through is when information is transformed to insights.
+			
+			At this stage, since our single data repository also contains historical information, organizations can now see if there are trends or patterns that will emerge from all the stored information.
+			From these trends and patterns, organizations will now be able to answer the questions,
+			“Why did it happen?”
+			“What could likely happen next?”
+			
+			The value of data will not be fully realized until organizations act on the insights that emerge from the analysis that has been done so far.
+			The transformation then of insights to imperatives or actionable insights is the last transformation that data goes through.
+			Given the insights of what could likely happen next, Analytics can develop various options that will help organizations answer the question, “What should be done next?”
+			
+			From these options, organizations can decide on a course of action for their organization.
+			This is the data value chain: from the birth of data, to information (answering the question, “What happened?”), to insights (answering the questions, “Why did it happen?
+			What could likely happen next?”), to imperatives or actionable insights (answering the question, “What should be done next?”).
+
+
+*/
